9f38946013a5ae5388269e8abb578245b325507c
Karan Jamwal <karanjamwal2004@gmail.com>
2026-04-24 15:25:04 +0530

    feat: implement responsive mobile navigation with new sheet and layout components

--- client/src/components/layout/CustomerLayout.tsx
+++ client/src/components/layout/CustomerLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import { CustomerNavbar } from "../customer/common/desktop-navbar";
 
 export const CustomerLayout = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
+      <CustomerNavbar />
       {/* Navbar */}
       <main className="mx-auto max-x-7xl px-4 py-8">
         <Outlet />
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,7 +2,7 @@ import { HomePage } from "./pages/customer/Home";
 import { SignUpPage } from "./pages/auth/SignUp";
 import { SignInPage } from "./pages/auth/SignIn";
 import { createBrowserRouter } from "react-router";
-import { CustomerLayout } from "./components/customer/CustomerLayout";
+import { CustomerLayout } from "./components/layout/CustomerLayout";
 import { PublicOnlyLayout } from "./components/auth/PublicOnlyLayout";
 import { ProtectedLayout } from "./components/auth/ProtectedLayout";
 import { ProfilePage } from "./pages/customer/Profile";
